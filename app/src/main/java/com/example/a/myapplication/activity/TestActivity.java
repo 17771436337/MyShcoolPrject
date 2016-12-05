248c70e57eb3481ebda7a3ffd0b9f11e11d60c19
PH <627695222@qq.com>
2016-12-05 22:48:30 +0800

    加载状态

--- app/src/main/java/com/example/a/myapplication/activity/TestActivity.java
+++ app/src/main/java/com/example/a/myapplication/activity/TestActivity.java
@@ -1,11 +1,13 @@
 package com.example.a.myapplication.activity;
 
 
+import android.view.View;
 import com.example.a.myapplication.BaseActivity;
 import com.example.a.myapplication.R;
+import com.example.a.myapplication.view.LoadingPagerActivity;
 
 public class TestActivity extends BaseActivity {
-
+    private LoadingPagerActivity mLoadingPager;
 
     @Override
     protected int getLayoutID() {
@@ -15,7 +17,18 @@ public class TestActivity extends BaseActivity {
     @Override
     protected void initView() {
         mTitleView.setTitle("test");
-
+    /*    mLoadingPager = new LoadingPagerActivity(this) {
+            @Override
+            protected View initSuccessView() {
+                //返回成功的页面
+                return onSucccessView();
+            }
+            @Override
+            protected LoadedResult onLoadData() {
+                return performLoadingData();
+            }
+        };
+        setContentView(mLoadingPager);*/
     }
 
     @Override
