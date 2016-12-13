3319cb67a85bfbcb8d18d0457bd6569c9511f88e
cmy <1020233514@qq.com>
2016-12-13 10:09:24 +0800

    设计师界面

--- app/src/main/java/com/example/a/myapplication/fragment/FindFragment.java
+++ app/src/main/java/com/example/a/myapplication/fragment/FindFragment.java
@@ -3,27 +3,22 @@ package com.example.a.myapplication.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.ViewUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.a.myapplication.BaseFragment;
 import com.example.a.myapplication.R;
-import com.example.a.myapplication.view.LoadingPager;
 
 
 /**
  * 发现
  * */
-public class FindFragment extends BaseFragment {
-	View mView;
+public class FindFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 							 @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
-		mView=inflater.inflate(R.layout.fragment_find, container, false);
-		return mView;
+		return inflater.inflate(R.layout.fragment_find, container, false);
 
 	}
 
@@ -33,15 +28,6 @@ public class FindFragment extends BaseFragment {
 
 	}
 
-	@Override
-	protected View onLoadSuccessView() {
-		return mView;
-	}
-
-	@Override
-	protected LoadingPager.LoadedResult onLoadingData() {
-		return LoadingPager.LoadedResult.SUCCESS;
-	}
 
 
 }
