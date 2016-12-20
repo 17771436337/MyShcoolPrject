d054438b79476630d6f716ecc5898b0d54046dda
PH <627695222@qq.com>
2016-12-20 14:38:30 +0800

    配置信息

--- app/src/main/java/com/example/a/myapplication/util/Config.java
+++ app/src/main/java/com/example/a/myapplication/util/Config.java
@@ -5,4 +5,11 @@ package com.example.a.myapplication.util;
  */
 public class Config {
     public final static String hostString = "http://115.28.94.239/";
+
+    /**
+     * 造型师列表
+     */
+    public final static String QITTMELIST = hostString+"wardrobe/code/wardrode/index.php?s=/App/Item/qItemList";
+
+
 }
