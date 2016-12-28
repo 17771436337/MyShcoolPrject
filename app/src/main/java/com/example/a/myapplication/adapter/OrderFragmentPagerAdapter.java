62d5bb224b87bd3346057fd95e0e4023490b32b3
PH <627695222@qq.com>
2016-12-28 19:18:38 +0800

    公用方法类

---
+++ app/src/main/java/com/example/a/myapplication/adapter/OrderFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.a.myapplication.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.a.myapplication.fragment.OrderFragment;
+
+/**
+ * Created by Administrator on 2016/12/22.
+ */
+public class OrderFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private String[] titles = new String[]{"全部", "待付款", "待发货", "待收货"};
+    private Context context;
+
+    public OrderFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return OrderFragment.newInstance(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+}
