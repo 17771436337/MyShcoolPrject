a73913ce8a3e2ffb2ad4e914428f9f4de6dadf85
PH <627695222@qq.com>
2016-12-28 18:58:36 +0800

    公用方法类

---
+++ app/src/main/java/com/example/a/myapplication/util/OnDataChangeListener.java
@@ -0,0 +1,11 @@
+package com.example.a.myapplication.util;
+
+import java.util.Map;
+
+/**
+ * Created by a on 2016/12/28.
+ */
+
+public interface OnDataChangeListener {
+    public void  onDataChagne(Map<String,String> parm);
+}
